272c1c2d95276f16e8b8e23fefbdf844a4c076a5
Jackie Mutua <jkm.mutua@gmail.com>
2014-08-14 08:01:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-angular.module('BuildsGraph',['BuildsGraph']);
-	function GraphData($scope) {
-		var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Build Per Week');
-        data.addColumn('number', 'Success Rate (%)');
-        data.addRows([  
-          ['Wk 1', 70],
-          ['Wk 2', 68],
-          ['Wk 3', 90],
-          ['Wk 4', 74],
-          ['Wk 5', 89],
-          ['Wk 6', 67], 
-          ['Wk 7', 100]
-        ]);
+angular.module('BuildsGraph', ['BuildsGraph']);
 
-        var options = {'title':'Statistical Graph',
-                       'width':640,
-                       'height':480,
-                       'is3D':true};
+function GraphData($scope) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Build Per Week');
+    data.addColumn('number', 'Success Rate (%)');
+    data.addRows([
+        ['Wk 1', 70],
+        ['Wk 2', 68],
+        ['Wk 3', 90],
+        ['Wk 4', 74],
+        ['Wk 5', 89],
+        ['Wk 6', 67],
+        ['Wk 7', 100]
+    ]);
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
- 	}
+    var options = {
+        'title': 'Statistical Graph',
+        'width': 640,
+        'height': 480,
+        'is3D': true
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
